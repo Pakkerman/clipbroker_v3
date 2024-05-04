805e0cf88b698833bab62edc71afe2b5f66bc270
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-05-04 16:54:37 +0800

    feat: assign new board from the server then pass new id down to client

--- src/app/_components/BoardIdentifier.tsx
+++ src/app/_components/BoardIdentifier.tsx
@@ -2,20 +2,31 @@
 
 import { api } from "~/trpc/react";
 import { useClipboard } from "../api/context/ClipboardContext";
+import { useRouter } from "next/navigation";
 
-export function BoardIdentifier() {
-  const { id } = useClipboard();
+export function BoardIdentifier({ newId }: { newId: string }) {
+  const router = useRouter();
+  // const { isLoading, data } = api.user.getOrInsertOne.useQuery({
+  //   id: randomId,
+  // });
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       please input board id
-      <form className="flex flex-col" onSubmit={() => {}}>
+      <form
+        className="flex flex-col"
+        action={async (formData) => {
+          const id = formData.get("id") || newId;
+          router.push(`/${id}`);
+        }}
+      >
         <input
-          className="rounded-md p-2 text-black"
+          className="rounded-md p-2 text-center text-black "
           type="text"
+          name="id"
           minLength={6}
           maxLength={6}
-          placeholder={id}
+          placeholder={newId}
         />
         <button type="submit">Enter</button>
       </form>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,23 @@ import { CreateText } from "./_components/create-text";
 import { Texts } from "./_components/texts";
 import { BoardIdentifier } from "./_components/BoardIdentifier";
 import { generateId } from "~/utils/utils";
+import {
+  getBoardIds,
+  getNewId,
+  validNewBoardId,
+} from "~/server/db/functions/user";
+import { cookies } from "next/headers";
 
-export default function Home() {
-  const randomId = generateId();
+export default async function Home() {
+  const newId = await getNewId();
 
   return (
     <section className="flex h-full flex-col items-center justify-center">
       <h1 className="text-3xl">clipbroker_v3</h1>
       <div className="flex grow flex-col items-center justify-center gap-4">
-        <BoardIdentifier randomId={randomId} />
+        <BoardIdentifier newId={newId} />
       </div>
+      <pre>{JSON.stringify(newId, null, 2)}</pre>
     </section>
   );
 }
