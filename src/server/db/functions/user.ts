18668a5e90cc162c6b90498690cce9de5fcdc818
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-05-04 16:53:00 +0800

    feat: add server side query function

---
+++ src/server/db/functions/user.ts
@@ -0,0 +1,35 @@
+import "server-only";
+//
+import { eq } from "drizzle-orm";
+// import { auth } from "@clerk/nextjs/server";
+// import { redirect } from "next/navigation";
+// import { revalidatePath } from "next/cache";
+
+import { db } from "~/server/db";
+import { users } from "~/server/db/schema";
+import { generateId } from "~/utils/utils";
+
+export async function getBoardIds() {
+  const data = await db.select().from(users);
+  return data;
+}
+
+export async function validNewBoardId(id: string): Promise<boolean> {
+  const data = await db.query.users.findFirst({
+    where: eq(users.clipboardId, id),
+  });
+
+  return data ? true : false;
+}
+
+export async function getNewId(): Promise<string> {
+  let rand = generateId();
+
+  while (
+    await db.query.users.findFirst({ where: eq(users.clipboardId, rand) })
+  ) {
+    rand = generateId();
+  }
+
+  return rand;
+}
